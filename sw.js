8c513a3040514ce056839608daffdadf8d8a532f
padmashreerameah-ship-it <padmashreerameah@gmail.com>
2026-04-16 12:29:08 +0530

    final pwa fix

--- sw.js
+++ sw.js
@@ -1,20 +1,23 @@
 const CACHE_NAME = "quiz-app-v1";
 
 self.addEventListener("install", event => {
-event.waitUntil(
-caches.open(CACHE_NAME).then(cache => {
-return cache.addAll([
-"index.html",
-"manifest.json"
-]);
-})
-);
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json",
+        "/icon-192.png",
+        "/icon-512.png"
+      ]);
+    })
+  );
 });
 
 self.addEventListener("fetch", event => {
-event.respondWith(
-caches.match(event.request).then(response => {
-return response || fetch(event.request);
-})
-);
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
